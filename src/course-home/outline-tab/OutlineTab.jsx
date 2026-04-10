fef362efd45a7e87d06b8e1232fe09bfba509844
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-04-10 15:53:20 +0530

    css changes

--- src/course-home/outline-tab/OutlineTab.jsx
+++ src/course-home/outline-tab/OutlineTab.jsx
@@ -119,7 +119,7 @@ const OutlineTab = () => {
   return (
     <>
       <div data-learner-type={learnerType} className="row w-100 mx-0 my-3 justify-content-between">
-        <div className="col-12 col-sm-auto p-0">
+        <div className="col-12 col-sm-auto p-0 custom-heading-box">
           <div role="heading" aria-level="1" className="h2">{title}</div>
         </div>
       </div>
