3c01cca41f768b44af743806c32968ea25051658
Aman-K-Ray <aman.kumar.a.ray@accenture.com>
2026-04-10 17:51:13 +0530

    import fix

--- src/course-tabs/CourseTabsNavigation.jsx
+++ src/course-tabs/CourseTabsNavigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import classNames from 'classnames';
